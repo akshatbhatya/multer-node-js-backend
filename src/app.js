f165695b577834d42aec9e1939d7f0345cdea64c
akshat <akshatbhatya@gmail.com>
2024-04-02 20:41:58 +0530

    app is configure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import  Express  from "express";
-import cors from "cors"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app=Express();
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
 }))
+app.use(Express.json({limit:"16kb"}));
 
+app.use(Express.urlencoded({extended:true,limit:"16kb"}));
+app.use(Express.static("public"));
+app.use(cookieParser());
 export{app};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import dotenv from"dotenv";
 import { connectDatabase } from "./db";
+import { app } from "./app";
 dotenv.config({path:"./env"});
-connectDatabase();
+connectDatabase().then(()=>{
+    app.listen(process.env.PORT,(req,res)=>{
+        console.log("server is running");
+
+    })
+}).catch((err)=>console.log(err))
