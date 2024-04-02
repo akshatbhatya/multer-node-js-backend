a70d70be2de9803d446ea1bd18eace55db5bcf34
akshat <akshatbhatya@gmail.com>
2024-04-02 20:49:56 +0530

    add space in code

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-import  Express  from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-const app=Express();
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-}))
-app.use(Express.json({limit:"16kb"}));
+import Express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app = Express()
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+)
 
-app.use(Express.urlencoded({extended:true,limit:"16kb"}));
-app.use(Express.static("public"));
-app.use(cookieParser());
-export default app;
+app.use(Express.json({ limit: "16kb" }))
+app.use(Express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(Express.static("public"))
+app.use(cookieParser())
+
+export default app
