e0b4a538e67e05724602647190ca1f36d2c77b54
akshat <akshatbhatya@gmail.com>
2024-04-03 15:04:20 +0530

    configure api error

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,10 +1,12 @@
 class ApiError extends Error{
-    constructor(stack,message,error,status){
+    constructor(stack,message,error,status,data){
         super(message)
         this.stack=stack,
         this.message=message,
         this.error=error,
-        this.stack=stack
+        this.stack=stack,
+        this.data=null
+        this.status=status
 
         if(!stack){
             Error.captureStackTrace(this,this.constructor);
